e7115564223342196174cffb29dc7badfee9ef55
unknown <ashleytorio@gmail.com>
2024-09-10 10:49:20 +0800

    Subtract

--- calculator.java
+++ calculator.java
@@ -6,4 +6,11 @@ public class calculator{
     public static int add(int a, int b){
         return a + b;
     }
+
+public class calculator{
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+
+}
 }
