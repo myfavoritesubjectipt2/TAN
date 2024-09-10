b7dc9f3aff2e30093d40442f829bc262eb561522
unknown <ashleytorio@gmail.com>
2024-09-10 10:57:03 +0800

    MODULUS

--- calculator.java
+++ calculator.java
@@ -19,7 +19,15 @@ public class calculator{
 
 public class calculator{
         public static int divide(int a, int b){
+            if(b == 0){
+                throw new ArithmeticException("Cannot divided by zero.");
+            }
                 return a/b;
+    }
+
+    public class calculator{
+        public static int quotient(int a, int b){
+                return a%b;
     }
     
 }
